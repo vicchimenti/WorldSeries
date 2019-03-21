7afe2afdd0f4e3ae1d6a21b702787dfe196a19d8
Vic Chimenti <chimenti@seattleu.edu>
2019-03-21 16:42:56 -0700

    Initial commit

---
+++ world_series.py
@@ -0,0 +1,7 @@
+'''
+Probability Predictor
+Evaluates probability of winning w games in a series of n games
+Requires the input of the number of games in series n and
+the probability of winning one game p
+Assumes that p is the same for each game
+'''

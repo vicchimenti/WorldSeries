6e99950c990598a0202d3e0e68b3cf4b8d05ab2a
Vic Chimenti <chimenti@seattleu.edu>
2019-03-21 16:54:30 -0700

    draft

--- world_series.py
+++ world_series.py
@@ -5,3 +5,30 @@ Requires the input of the number of games in series n and
 the probability of winning one game p
 Assumes that p is the same for each game
 '''
+
+
+# world series function accepts number of games in series and win probability
+def world_series(n, p):
+
+    # number of wins needed to win the series
+    wins = n//2 + 1
+    q = 1 - p
+
+    # build probability matrix
+    P = [1 for i in range(wins + 1)]
+    P[0] = 0
+
+    # calculate matrix
+    for i in range (1, wins + 1):
+        for j in range(1, wins + 1):
+            P[j] = p*P[j] + q*P[j-1]
+
+    return P[j]
+
+
+# get the parameters from the user
+games = input('Enter the number of games in the series: ')
+probability = input ('Enter the chances of your team winning as a positive decimal less than or equal to one: ')
+
+
+
